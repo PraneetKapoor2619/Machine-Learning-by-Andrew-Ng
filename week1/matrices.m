3ccff0a722f110fb7fc326602438f55b3a3bbf3e
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-10 12:35:34 +0530

    Matrix-vector multiplication

--- week1/matrices.m
+++ week1/matrices.m
@@ -1,21 +1,8 @@
-% Initialize matrix A and B 
-A = [1, 2, 4; 5, 3, 2]
-B = [1, 3, 4; 1, 1, 1]
+% Initialize matrix A 
+A = [1, 2, 3; 4, 5, 6;7, 8, 9] 
 
-% Initialize constant s 
-s = 2
+% Initialize vector v 
+v = [1; 1; 1] 
 
-% See how element-wise addition works
-add_AB = A + B 
-
-% See how element-wise subtraction works
-sub_AB = A - B
-
-% See how scalar multiplication works
-mult_As = A * s
-
-% Divide A by s
-div_As = A / s
-
-% What happens if we have a Matrix + scalar?
-add_As = A + s
+% Multiply A * v
+Av = A * v
