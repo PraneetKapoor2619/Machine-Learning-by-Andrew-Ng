c62d616d84411558850c32b335738c59b301701d
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-10 12:52:57 +0530

    Matrix multiplication properties and identity matrix

--- week1/matrices.m
+++ week1/matrices.m
@@ -1,10 +1,22 @@
-% Initialize a 3 by 2 matrix 
-A = [1, 2; 3, 4;5, 6]
+% Initialize random matrices A and B 
+A = [1,2;4,5]
+B = [1,1;0,2]
 
-% Initialize a 2 by 1 matrix 
-B = [1; 2] 
+% Initialize a 2 by 2 identity matrix
+I = eye(2)
 
-% We expect a resulting matrix of (3 by 2)*(2 by 1) = (3 by 1) 
-mult_AB = A*B
+% The above notation is the same as I = [1,0;0,1]
 
-% Make sure you understand why we got that result
+% What happens when we multiply I*A ? 
+IA = I*A 
+
+% How about A*I ? 
+AI = A*I 
+
+% Compute A*B 
+AB = A*B 
+
+% Is it equal to B*A? 
+BA = B*A 
+
+% Note that IA = AI but AB != BA
